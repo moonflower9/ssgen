1568bfba66f20422e23e0b280d6390389921f619
moon <csteel217@gmail.com>
2026-02-17 13:10:44 +1300

    restore htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,69 @@
+from typing import override
+
+
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list["HTMLNode"] | None = None,
+        props: dict[str, str] | None = None,
+    ):
+        self.tag: str | None = tag
+        self.value: str | None = value
+        self.children: list[HTMLNode] | None = children
+        self.props: dict[str, str] | None = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError("to_html method not impplemented")
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    @override
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    @override
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    @override
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self, tag: str, children: list[HTMLNode], props: dict[str, str] | None = None
+    ):
+        super().__init__(tag, None, children, props)
+
+    @override
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html: str = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    @override
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
